ba6bd21ce6b5cf99958c0cef8738c792bc123a49
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-21 09:42:47 +0200

    Added Clickhouse

--- modules/Leveling/events.ts
+++
@@ -1,9 +0,0 @@
-import { Client, TextChannel } from 'discord.js';
-
-export default class Events {
-    private client: Client;
-
-    constructor(client: Client) {
-        this.client = client;
-    }
-}
