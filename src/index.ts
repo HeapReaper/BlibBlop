f32330e04f0b203de7265de14dc9d25c4b3540aa
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-25 15:52:11 +0200

    Add guild and member stats and boost count endpoint to API

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,46 @@ webApp.get('/user-statussen', (req: any, res: any) => {
 	res.json(userStatussen);
 });
 
+webApp.get('/stats/guild', async (req: any, res: any) => {
+	// Todo: make it update a exteral var once every minute
+	try {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+
+		const guild = await client.guilds.fetch(getEnv('GUILD_ID') as string);
+
+		if (!guild) return Logging.warn('Guild not found in index.ts stats/guild');
+
+		await guild.members.fetch();
+
+		const members = guild.members.cache.filter(member => !member.user.bot);
+		// @ts-ignore
+		const memberCount = members.size;
+		const onlineMembers = members.filter(member => member.presence?.status === 'online').size;
+
+		// @ts-ignore
+		const lastJoined = members.reduce((latest, member) => {
+			// @ts-ignore
+			if (!latest || (member.joinedAt && member.joinedAt > latest.joinedAt)) {
+				return member;
+			}
+			return latest;
+		}, null);
+
+		res.json({
+			memberCount: memberCount,
+			membersOnline: onlineMembers,
+			// @ts-ignore
+			lastJoined: formatTimestamp(lastJoined?.joinedTimestamp),
+			boostCount: guild.premiumSubscriptionCount,
+		});
+	} catch (error) {
+		Logging.warn(`Error in "/stats/guild/": ${error}`);
+	}
+
+})
+
 webApp.listen(PORT, () => {
-	console.log(`Web server running at http://localhost:${PORT}`);
+	Logging.info(`API running at http://localhost:${PORT}`);
 });
 
 
@@ -81,5 +119,17 @@ client.on('ready', async client => {
 cron.schedule('* * * * *', async (): Promise<void> => {
 	await usersOnline(client);
 })
+
+function formatTimestamp(timestamp: any) {
+	const date = new Date(timestamp);
+	const dd = String(date.getDate()).padStart(2, '0');
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const yy = String(date.getFullYear()).slice(-2);
+	const h = date.getHours();
+	const min = String(date.getMinutes()).padStart(2, '0');
+
+	return `${dd}-${mm}-${yy} ${h}:${min}`;
+}
+
 
 void client.login(getEnv('DISCORD_TOKEN'));
