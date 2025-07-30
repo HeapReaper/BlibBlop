5b0ac2d7f2023406053eb7d44701b476f27f2aa6
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-30 10:24:30 +0200

    Removed unnecessary link in Reminders task description

--- modules/Reminders/tasks.ts
+++ modules/Reminders/tasks.ts
@@ -23,7 +23,7 @@ export default class Tasks {
 		const embed = new EmbedBuilder()
 			.setColor(0x2563EB)
 			.setTitle('RC Garage Herinnering')
-			.setDescription('Heb jij al een review achtergelaten op [Disboard](https://disboard.org/nl/server/1350811442856726559)? Je helpt de server daarmee groeien en nog meer RC-liefhebbers bereiken!')
+			.setDescription('Heb jij al een review achtergelaten op [Disboard](https://disboard.org/nl/server/1350811442856726559)? Je helpt de server daarmee groeien!')
 
 		await this.generalChannel.send({ embeds: [embed] });
 	}
