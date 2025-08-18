2b1e07c9e7085bd03e1e86dc46963b873380b17f
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-18 16:43:52 +0200

    Removed unused workflow, updated review reminder text

--- modules/Reminders/tasks.ts
+++ modules/Reminders/tasks.ts
@@ -23,7 +23,7 @@ export default class Tasks {
 		const embed = new EmbedBuilder()
 			.setColor(0x2563EB)
 			.setTitle('RC Garage Herinnering')
-			.setDescription('Heb jij al een review achtergelaten op [Disboard](https://disboard.org/nl/server/1350811442856726559)? Je helpt de server daarmee groeien!')
+			.setDescription('Heb jij al een review achtergelaten op [Disboard](https://disboard.org/nl/server/1350811442856726559) of [Top.GG](https://top.gg/discord/servers/744654584256151552?s=01e8761801f01)?\nJe helpt de server daarmee groeien!')
 
 		await this.generalChannel.send({ embeds: [embed] });
 	}
