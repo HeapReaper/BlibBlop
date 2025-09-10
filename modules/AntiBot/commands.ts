964eaf42234cf3b5d014eebfe1a61575fbfed85f
HeapReaper <kelvin@aerobytes.nl>
2025-09-10 12:06:53 +0200

    Removed anti-bot

--- modules/AntiBot/commands.ts
+++
@@ -1,5 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const commands = [
-
-].map(commands => commands.toJSON());
--- modules/AntiBot/tasks.ts
+++
@@ -1,9 +0,0 @@
-import { Client, TextChannel } from 'discord.js';
-
-export default class Tasks {
-	private client: Client;
-
-    constructor(client: Client) {
-		this.client = client;
-	}
-}
