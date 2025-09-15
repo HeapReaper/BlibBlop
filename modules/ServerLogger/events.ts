d0fd546d050cee6be1543157bc06deadc3b6743c
HeapReaper <kelvin@aerobytes.nl>
2025-09-15 19:52:00 +0200

    Fixed member join log message

--- modules/ServerLogger/events.ts
+++ modules/ServerLogger/events.ts
@@ -25,6 +25,7 @@ import db from '@utils/knex';
 import os from 'os';
 import { isBot } from '@utils/isBot';
 import {LogToServer} from '@utils/logToServer.ts';
+//import { Faker } from "@heapreaper/discordfaker";
 
 export async function externalLogToServer(message: string, client: Client) {
 	const logChannel = client.channels.cache.get(<string>getEnv('LOG')) as TextChannel;
@@ -60,6 +61,8 @@ export default class Events {
 		this.reactionEvents();
 		this.voiceChannelEvents();
 		void this.memberEvents();
+
+		//Faker.memberAdd(this.client)
 	}
 
 	async bootEvent(): Promise<void> {
@@ -397,9 +400,23 @@ export default class Events {
 	 * @return {Promise<void>}
 	 */
 	async memberEvents(): Promise<void> {
-		// On member join is handles by invite tracker
+		this.client.on(discordEvents.GuildMemberAdd, async (member: GuildMember | PartialGuildMember): Promise<void> => {
+			Logging.info(`A user with the name ${member.user.displayName} joined this Discord!`);
+
+			const memberEventEmbed = new EmbedBuilder()
+				.setColor(Color.Red)
+				.setTitle('Lid bijgekomen')
+				.setThumbnail('attachment://user.png')
+				.addFields(
+					{ name: 'Gebruiker:', value: `<@${member.id ?? 'Fout'}>` },
+					{ name: 'Lid sinds:', value: `${(member.joinedAt?.toLocaleString('nl-NL') ?? 'Fout') }` },
+				);
+
+			await this.logChannel.send({ embeds: [memberEventEmbed], files: [this.userIcon] });
+		});
+
 		this.client.on(discordEvents.GuildMemberRemove, async (member: GuildMember|PartialGuildMember): Promise<void> => {
-			Logging.info('A user left this Discord!');
+			Logging.info(`A user with the name ${member.user.displayName} left this Discord!`);
 
 			const memberEventEmbed = new EmbedBuilder()
 				.setColor(Color.Red)
