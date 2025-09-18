4b9c04104b8ce827b56703a50b4b037918940f6b
HeapReaper <kelvin@aerobytes.nl>
2025-09-18 08:45:32 +0200

    Refactored env helper

--- utils/env.ts
+++ utils/env.ts
@@ -1,18 +1,7 @@
-// @ts-ignore
-import dotenv from 'dotenv';
-import * as process from 'node:process';
+import * as process from "node:process";
 
-dotenv.config({ path: '../.env' });
+export const getEnv = (key: string, fallback: string = ""): string|undefined => {
+	const envValue: string | number | boolean | undefined = process.env[key];
 
-/**
- * Gets an environment value from .env
- *
- * @param {string} key - The environment key. Example: DISCORD_TOKEN.
- * @param {string} fallback - Fallback value. Defaults to an empty string.
- * @return {string|number|boolean|undefined} - Returns a string or undefined.
- */
-export const getEnv = (key: string, fallback: string = ''): string|undefined => {
-	const envValue: string|number|boolean|undefined = process.env[key];
-	
 	return envValue ? envValue : fallback;
 }
