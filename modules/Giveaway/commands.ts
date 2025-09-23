65417feb64f7f378c2f8f6e1ee95619a8fa58847
HeapReaper <kelvin@aerobytes.nl>
2025-09-23 10:18:47 +0200

    Added commands and basic listener

--- modules/Giveaway/commands.ts
+++ modules/Giveaway/commands.ts
@@ -1,5 +1,79 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} from "discord.js";
 
 export const commands = [
-
-].map(commands => commands.toJSON());
+  new SlashCommandBuilder()
+    .setName("giveaway")
+    .setDescription("Commands voor het giveaway systeem")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addSubcommand(sub =>
+      sub
+        .setName("aanmaken")
+        .setDescription("Maak een giveaway aan")
+        .addIntegerOption(option =>
+          option
+            .setName("dag")
+            .setDescription("Op welke dag eindigt de giveaway?")
+            .setRequired(true)
+        )
+        .addIntegerOption(option =>
+          option
+            .setName("maand")
+            .setDescription("In welke maand eindigt de giveaway?")
+            .setRequired(true)
+        )
+        .addIntegerOption(option =>
+          option
+            .setName("jaar")
+            .setDescription("In welk jaar eindigt de giveaway?")
+            .setRequired(true)
+        )
+        .addStringOption(option => // tijd beter als string, geen integer
+          option
+            .setName("tijd")
+            .setDescription("Hoelaat eindigt de giveaway? (ex. 16:24)")
+            .setRequired(true)
+        )
+        .addIntegerOption(option =>
+          option
+            .setName("aantal_winnaars")
+            .setDescription("Hoeveel winnaars zijn er?")
+            .setRequired(true)
+        )
+        .addStringOption(option =>
+          option
+            .setName("prijs")
+            .setDescription("Wat is de prijs van deze giveaway?")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(sub =>
+      sub
+        .setName("verwijder")
+        .setDescription("Verwijder een giveaway")
+        .addStringOption(option =>
+          option
+            .setName("id")
+            .setDescription("Vul de giveaway ID in")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(sub =>
+      sub
+        .setName("lijst")
+        .setDescription("Zie alle lopende giveaways")
+    )
+    .addSubcommand(sub =>
+      sub
+        .setName("reroll")
+        .setDescription("Kies een 2e winnaar uit")
+        .addStringOption(option =>
+          option
+            .setName("id")
+            .setDescription("Vul de giveaway ID in")
+            .setRequired(true)
+        )
+    )
+].map(command => command.toJSON());
--- modules/Giveaway/commandsListener.ts
+++ modules/Giveaway/commandsListener.ts
@@ -1,4 +1,10 @@
-import { Client, Interaction, Events, MessageFlags} from "discord.js";
+import {
+	Client,
+	Interaction,
+	Events,
+	MessageFlags,
+	ChatInputCommandInteraction
+} from "discord.js";
 
 export default class CommandsListener {
 	private readonly client: Client;
@@ -7,8 +13,63 @@ export default class CommandsListener {
 		this.client = client;
 		void this.commandsListener();
 	}
-	
+
 	async commandsListener(): Promise<void> {
+		this.client.on(Events.InteractionCreate, async (interaction: ChatInputCommandInteraction): Promise<void> => {
+				if (!interaction.isCommand()) return;
+
+				const { commandName } = interaction;
+				const subCommandName: string | null = interaction.options.getSubcommand(true);
+
+				if (commandName !== "giveaway") return;
+
+				switch (subCommandName) {
+					case "aanmaken":
+						void this.create(interaction);
+						break;
+					case "verwijder":
+						void this.delete(interaction);
+						break;
+					case "reroll":
+						void this.reroll(interaction);
+						break;
+					case "lijst":
+						void this.list(interaction);
+						break;
+				}
+			}
+		);
+	}
+
+	async create(interaction: ChatInputCommandInteraction): Promise<void> {
+		if (!interaction.isCommand()) return;
+
+		const dag: number = interaction.options.getInteger("dag", true);
+		const maand: number = interaction.options.getInteger("maand", true);
+		const jaar: number = interaction.options.getInteger("jaar", true);
+		const tijd: string = interaction.options.getString("tijd", true);
+		const aantal_winnaars: number = interaction.options.getInteger("aantal_winnaars", true);
+		const prijs: string = interaction.options.getString("prijs", true);
+
+		//
+	}
+
+	async delete(interaction: ChatInputCommandInteraction): Promise<void> {
+		if (!interaction.isCommand()) return;
+
+		const id: string = interaction.options.getString("id", true);
+		//
+	}
+
+	async reroll(interaction: ChatInputCommandInteraction): Promise<void> {
+		if (!interaction.isCommand()) return;
+
+		const id: string = interaction.options.getString("id", true);
+		//
+	}
+
+	async list(interaction: ChatInputCommandInteraction): Promise<void> {
+		if (!interaction.isCommand()) return;
 		//
 	}
 }
